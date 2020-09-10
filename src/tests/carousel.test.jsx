4a2b4e98e8295322860dcc6c576d51044b01a71c
dnmeyer06 <dnmeyer06@gmail.com>
2020-09-10 18:36:07 +0800

    Continuting to set up testing infrastructure

---
+++ src/tests/carousel.test.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import Carousel from '../carousel';
+
+test('<Carousel />', () => {
+  expect(true).toBeTruthy();
+});
